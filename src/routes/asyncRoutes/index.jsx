3f2380d39209662b8c7d0f49dd0815a61e4cfd2f
Hanya Hesham Hamed Hassan <hanya.hamed@fintech.local>
2023-10-30 16:35:02 +0200

    updates

--- src/routes/asyncRoutes/index.jsx
+++ src/routes/asyncRoutes/index.jsx
@@ -1,27 +1,8 @@
 import { lazy } from "react";
-const TasksRecord = lazy(() => import("../../pages/Tasks/record"));
 const AllTasks = lazy(() => import("../../pages/Tasks/AllTasks"));
 const AddTasks = lazy(() => import("../../pages/Tasks/crud"));
 
 export const asyncRoutes = [
-  // {
-  //   path: "/tasks/record",
-  //   href: "/tasks/record",
-  //   exact: true,
-  //   handle: {
-  //     breadCrumb: [
-  //       {
-  //         title: "Home",
-  //         path: "/",
-  //         href: "/",
-  //       },
-  //       {
-  //         title: "Record",
-  //       },
-  //     ],
-  //   },
-  //   element: <TasksRecord />,
-  // },
   {
     path: "/tasks/all-tasks",
     href: "/tasks/all-tasks",
